c9a4e9c98b887766dcf5901f789063aac2c79e97
caiooliveirah09 <leelros@hotmail.com>
2023-05-25 15:39:17 -0300

    feat: stylized start menu with cyberpunk animation

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,3 +1,34 @@
 export default function Menu() {
-  return <h1 className="text-red-700 font-minecraft">Menu</h1>;
+  return (
+    <div className="bg-gray-900 h-screen flex flex-col justify-center items-center">
+      <header>
+        <h2
+          className="text-white font-playfairDisplay text-3xl font-extrabold"
+          style={{
+            textShadow:
+              "0 0 5px #fe00fe, 0 0 10px #fe00fe, 0 0 15px #fe00fe, 0 0 20px #fe00fe, 0 0 25px #fe00fe",
+          }}
+        >
+          welcome to my
+        </h2>
+        <h1
+          className="text-white font-minecraft text-9xl mt-8 mb-36 tracking-widest"
+          style={{
+            textShadow:
+              "0px 0px 3px #03bcf4, 0 0 5px #03bcf4, 0 0 20px #03bcf4, 0 0 30px #03bcf4",
+            WebkitBoxReflect: "below 1px linear-gradient(transparent, #0008)",
+            lineHeight: "0.60em",
+            animation: "cyberpunk 10s linear infinite",
+          }}
+        >
+          website
+        </h1>
+      </header>
+      <main>
+        <h3 className="text-[#defe47] font-mono animate-bounce text-xl">
+          press any button to start
+        </h3>
+      </main>
+    </div>
+  );
 }
